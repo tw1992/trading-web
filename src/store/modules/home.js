f7c47a5b9dd91d9a0455e5c51daf9364933a337e
ASxx <466865383@qq.com>
2018-06-23 19:08:37 +0800

    在store/modules增加home.js，用于显示各个交易市场的成交量

---
+++ src/store/modules/home.js
@@ -0,0 +1,13 @@
+const home = {
+  state: {
+    '24volumeCCC':0,
+    '24volumeETH':0,
+    '24volumeBTC':0,
+    '24volumeOMG':0,
+    '24volumeDOGE':0,
+    '24volumeWWW':0,
+    '24volumeRNG':0
+  },
+}
+
+export default home
